328426ea07981cfeba721cc5502a6270cce6334f
gansergey <gaaan@outlook.com>
2021-10-04 19:28:12 +0300

    HW10

--- app/src/main/java/com/example/newmyweaterproject/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/newmyweaterproject/ui/main/view/MainActivity.kt
@@ -1,19 +1,112 @@
 package com.example.newmyweaterproject.ui.main.view
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.IntentFilter
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.ConnectivityManager.CONNECTIVITY_ACTION
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.newmyweaterproject.R
 import com.example.newmyweaterproject.databinding.MainActivityBinding
 import com.example.newmyweaterproject.ui.main.model.MainBroadcastReceiver
+import java.io.IOException
 
+private const val REFRESH_PERIOD = 60000L
+private const val MINIMAL_DISTANCE = 100f
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MainBroadcastReceiver()
+
+    private val permissionGeoResult = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            getLocation()
+            !ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            Toast.makeText(
+                this,
+                resources.getText(R.string.warning_permission), Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.getProvider(LocationManager.GPS_PROVIDER)?.let {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    REFRESH_PERIOD,
+                    MINIMAL_DISTANCE,
+                    object : LocationListener {
+
+                        override fun onLocationChanged(location: Location) {
+                            getAddressByLocation(location)
+                        }
+
+                        override fun onStatusChanged(
+                            provider: String?,
+                            status: Int,
+                            extras: Bundle?
+                        ) {
+                        }
+
+                        override fun onProviderEnabled(provider: String) {
+                        }
+
+                        override fun onProviderDisabled(provider: String) {
+                        }
+                    }
+                )
+            }
+        } else {
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)?.let {
+                getAddressByLocation(it)
+            } ?: Toast.makeText(
+                this, resources.getText(R.string.warning_gps_location), Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun getAddressByLocation(location: Location) {
+        val geocoder = Geocoder(this)
+
+        Thread {
+            try {
+                val address = geocoder.getFromLocation(
+                    location.latitude,
+                    location.longitude,
+                    1
+                )
+                binding.container.post {// post не из основного поток передать в основной поток
+                    AlertDialog.Builder(this)
+                        .setMessage(address[0].getAddressLine(0))
+                        .setCancelable(true)
+                        .show()
+                }
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+        }.start()
+    }
 
     private val binding: MainActivityBinding by lazy {
         MainActivityBinding.inflate(layoutInflater)
@@ -58,8 +151,26 @@ class MainActivity : AppCompatActivity() {
                 }
                 true
             }
+
+            R.id.menu_location -> {
+                permissionGeoResult.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                true
+            }
+
+            R.id.menu_location_map -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .replace(R.id.container, GoogleMapsFragment())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
+
+
     }
 
     override fun onDestroy() {
