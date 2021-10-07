17000ff6d3d2eed0e51aef75b55db535deea7163
gansergey <gaaan@outlook.com>
2021-10-07 17:29:13 +0300

    HW11

--- app/src/main/java/com/example/newmyweaterproject/ui/main/view/App.kt
+++ app/src/main/java/com/example/newmyweaterproject/ui/main/view/App.kt
@@ -1,15 +1,37 @@
 package com.example.newmyweaterproject.ui.main.view
 
 import android.app.Application
+import android.app.Notification
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.room.Room
+import com.example.newmyweaterproject.R
 import com.example.newmyweaterproject.ui.main.model.database.HistoryDao
 import com.example.newmyweaterproject.ui.main.model.database.HistoryDataBase
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         appInstance = this
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("Fetching FCM", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("Fetching FCM", token)
+            makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
     }
 
     companion object {
